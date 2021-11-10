ecbe16b882a7004e5c9c26723c48038c4df8e579
Mitchell <mwhyuen@gmail.com>
2021-11-10 11:18:52 +0200

    fix: styling with createGenerateClassName

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Landing from '../components/Landing';
 import Pricing from '../components/Pricing';
 
+
+const createGenerateClassName = createGenerateClassName({
+    productionPrefix: 'ma',
+})
 export default () => {
     return (
         <div>
